481d12e63070bfc0d0769c1753a991e2e7f7fbfe
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-19 10:43:08 +0000

    incluido poste

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -9,23 +9,29 @@ export function Home() {
   const [selectedColor, setSelectedColor] = useState("red");
 
   return (
-    <div className="container">
-      <div className="traffic-light">
-        <div
-          onClick={() => setSelectedColor("red")}
-          className={"light red" + (selectedColor === "red" ? " glow" : "")}
-        ></div>
+    <div className="traffic-container">
+      <div className="traffic-pole"></div>
 
-        <div
-          onClick={() => setSelectedColor("yellow")}
-          className={
-            "light yellow" + (selectedColor === "yellow" ? " glow" : "")
-          }
-        ></div>
-        <div
-          onClick={() => setSelectedColor("green")}
-          className={"light green" + (selectedColor === "green" ? " glow" : "")}
-        ></div>
+      <div className="container">
+        <div className="traffic-light">
+          <div
+            onClick={() => setSelectedColor("red")}
+            className={"light red" + (selectedColor === "red" ? " glow" : "")}
+          ></div>
+
+          <div
+            onClick={() => setSelectedColor("yellow")}
+            className={
+              "light yellow" + (selectedColor === "yellow" ? " glow" : "")
+            }
+          ></div>
+          <div
+            onClick={() => setSelectedColor("green")}
+            className={
+              "light green" + (selectedColor === "green" ? " glow" : "")
+            }
+          ></div>
+        </div>
       </div>
     </div>
   );
